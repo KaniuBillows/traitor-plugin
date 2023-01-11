3c3572fd78ca41aaab996e58190c90cd20c0c400
kaniu <kaniucc@qq.com>
2023-01-11 19:51:39 +0800

    async Executable

--- Executable.go
+++ Executable.go
@@ -3,6 +3,13 @@ package executor
 import "github.com/dop251/goja"
 
 type Executable interface {
+	GetName() string
+	ModuleLoader(runtime *goja.Runtime, module *goja.Object)
+	GetModule() Executable
+}
+
+type AsyncExecutable interface {
 	Require(exec *Executor) func(runtime *goja.Runtime, module *goja.Object)
 	GetName() string
+	GetModule() Executable
 }
