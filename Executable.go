dbad8f26690759605bca8314d4c74f6976086ba0
kaniu <kaniucc@qq.com>
2023-01-11 17:47:38 +0800

    change interface

--- Executable.go
+++ Executable.go
@@ -3,7 +3,6 @@ package executor
 import "github.com/dop251/goja"
 
 type Executable interface {
-	GetModule() Executable
 	Require(exec *Executor) func(runtime *goja.Runtime, module *goja.Object)
 	GetName() string
 }
