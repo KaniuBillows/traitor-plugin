d434ecfee3f6ecf5c3dd977bc194bb7b1de19c9c
kaniu <kaniucc@qq.com>
2023-01-11 19:56:16 +0800

    fix interface

--- Executable.go
+++ Executable.go
@@ -5,11 +5,9 @@ import "github.com/dop251/goja"
 type Executable interface {
 	GetName() string
 	ModuleLoader(runtime *goja.Runtime, module *goja.Object)
-	GetModule() Executable
 }
 
 type AsyncExecutable interface {
 	Require(exec *Executor) func(runtime *goja.Runtime, module *goja.Object)
 	GetName() string
-	GetModule() Executable
 }
